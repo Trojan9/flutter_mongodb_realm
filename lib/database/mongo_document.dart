a89969ac82fa053953226fb93aa259a09936158a
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2024-01-09 14:07:24 +0000

    amen

--- lib/database/mongo_document.dart
+++ lib/database/mongo_document.dart
@@ -57,8 +57,15 @@ class MongoDocument {
           if (map2[0].value is int)
             result =
                 DateTime.fromMillisecondsSinceEpoch(map2[0].value, isUtc: true);
-          else if (map2[0].value is String)
+          else if (map2[0].value is String) if (map2[0]
+                  .value[map2[0].value.length - 1]
+                  .toString() ==
+              "Z") {
+            var val = map2[0].value.substring(0, map2[0].value.length - 1);
+            result = DateTime.tryParse(val) ?? val;
+          } else {
             result = DateTime.tryParse(map2[0].value) ?? map2[0].value;
+          }
           break;
       }
     }
